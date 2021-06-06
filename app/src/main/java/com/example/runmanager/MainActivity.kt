98e02b5740b5d7a412f0821b9c92824d4bd7f59a
dean <dean.shannon@cqumail.com>
2021-06-06 11:16:33 +1000

    setup first screen

--- app/src/main/java/com/example/runmanager/MainActivity.kt
+++ app/src/main/java/com/example/runmanager/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.runmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val array = arrayOf("1", "2", "3")
+        val list = findViewById<ListView>(R.id.list)
+        val adapter : ArrayAdapter<*>
+        adapter  = ArrayAdapter(this, android.R.layout.simple_list_item_1, array)
+        list.adapter = adapter
     }
 }
