61d232853663e276dda883a71c851a894cd29320
dean <dean.shannon@cqumail.com>
2021-06-06 11:31:11 +1000

    open next screen

--- app/src/main/java/com/example/runmanager/MainActivity.kt
+++ app/src/main/java/com/example/runmanager/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.runmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 
@@ -14,5 +16,10 @@ class MainActivity : AppCompatActivity() {
         val adapter : ArrayAdapter<*>
         adapter  = ArrayAdapter(this, android.R.layout.simple_list_item_1, array)
         list.adapter = adapter
+        list.onItemClickListener  = AdapterView.OnItemClickListener { parent, view, position, id ->
+            val intent :Intent = Intent(applicationContext, ListDetail::class.java)
+            startActivity(intent)
+        }
+
     }
 }
